992856b86c7dccea913d2a1e599e54fb54a5b2f1
Marcos Oliveira <oliveira.marcos@engenharia.ufjf.br>
2023-07-07 15:39:46 -0300

    Create service layer

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import service
 import models
 import schemas
 from database import SessionLocal, engine
+from services.user import UserModelService
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -21,21 +22,31 @@ def get_db():
 
 @app.post("/users/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = service.get_user_by_email(db, email=user.email)
+    db_user = UserModelService.get_user_by_email(
+        db=db,
+        email=user.email
+    )
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
     return service.create_user(db=db, user=user)
 
 
 @app.get("/users/", response_model=list[schemas.User])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = service.get_users(db, skip=skip, limit=limit)
+def read_users(offset: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = UserModelService.get_users(
+        db=db,
+        offset=offset,
+        limit=limit
+    )
     return users
 
 
 @app.get("/users/{user_id}", response_model=schemas.User)
 def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = service.get_user(db, user_id=user_id)
+    db_user = UserModelService.get_user(
+        db=db,
+        user_id=user_id
+    )
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
---
+++ services/user.py
@@ -0,0 +1,30 @@
+from sqlalchemy.orm import Session
+from datetime import datetime
+import models
+import schemas
+
+
+class UserModelService:
+    model = models.User
+
+    @classmethod
+    def get_user(cls, db: Session, user_id: int):
+        return db.query(cls.model).filter(cls.model.id == user_id).first()
+
+    @classmethod
+    def get_user_by_email(cls, db: Session, email: str):
+        return db.query(cls.model).filter(cls.model.email == email).first()
+
+    @classmethod
+    def get_users(cls, db: Session, offset: int = 0, limit: int = 100):
+        return db.query(cls.model).offset(offset).limit(limit).all()
+
+    @classmethod
+    def create_user(cls, db: Session, user: schemas.UserCreate):
+        fake_hashed_password = user.password + "notreallyhashed"
+        db_user = cls.model(email=user.email, hashed_password=fake_hashed_password)
+        db_user.registered_on = datetime.now()
+        db.add(db_user)
+        db.commit()
+        db.refresh(db_user)
+        return db_user
