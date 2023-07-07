a64123dffe90ef49bdaf58b3c9bdef0d02f80340
Marcos Oliveira <oliveira.marcos@engenharia.ufjf.br>
2023-07-07 15:19:34 -0300

    Project kickoff with FastAPI: start user model and schemas

---
+++ schemas.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+
+
+class UserBase(BaseModel):
+    email: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+    is_active: bool
+
+    class Config:
+        orm_mode = True
+        # Pydantic's orm_mode will tell the Pydantic model to read the data even if it is not a dict,
+        # but an ORM model (or any other arbitrary object with attributes).
