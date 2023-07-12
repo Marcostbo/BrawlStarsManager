7c931f9a8723814ee0994282250349c0f7f4947f
Marcos Oliveira <oliveira.marcos@engenharia.ufjf.br>
2023-07-12 20:08:57 -0300

    Async io engine

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import sessionmaker
 
 # Currently, using SQLite. Moving to Postgres later
@@ -8,6 +9,11 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./sqlapp.db"
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
+
+# async_engine = create_async_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+# )
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
